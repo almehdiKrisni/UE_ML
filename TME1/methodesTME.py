06d651d918c682bebd14ec5d8f269afcc61c6fe7
unknown <almehdi.krisni@gmail.com>
2022-03-22 01:11:09 +0100

    DTree work started

--- TME1/methodesTME.py
+++ TME1/methodesTME.py
@@ -43,12 +43,36 @@ datax = data [:,:32]
 datay = np.array([1 if x [33] > 6.5 else -1 for x in data ])
 
 #############################################################################################
+# Quelques expériences préliminaires
+#############################################################################################
+
+from sklearn.tree import export_graphviz
+from sklearn.tree import DecisionTreeClassifier as DTree
+import pydotplus
+
+id2genre = [x[1] for x in sorted(fields.items())[: -2]]
+dt = DTree()
+dt.max_depth = 5 # on fixe la taille max de l ’ arbre a 5
+dt.min_samples_split = 2 # nombre minimum d ’ exemples pour spliter un noeud
+dt.fit(datax, datay)
+dt.predict(datax[:5,:])
+# print(dt.score(datax, datay))
+# utiliser http://www.webgraphviz.com/ par exemple ou https://dreampuf.github.io/Graphvizexport_graphviz ( dt , out_file ="/tmp / tree . dot ", feature_names = id2genre )
+# ou avec pydotplus
+tdot = export_graphviz(dt, feature_names = id2genre )
+pydotplus.graph_from_dot_data(tdot).write_pdf("tree.pdf")
+
+#############################################################################################
 # Partie EXEC - Tests
 #############################################################################################
 
-# Test de l'entropie
-vecTest1 = [random.randint(0, 10) for _ in range(5)]
-vecTest2 = [1, 1, 1, 1, 1]
+# Variables de contrôle des tests
+t1 = 0
 
-print("Vecteur :", vecTest1, "\tEntropie :", entropie(vecTest1))
-print("Vecteur :", vecTest2, "\tEntropie :", entropie(vecTest2))
+# Test de l'entropie
+if (t1 == 1) :
+    vecTest1 = [random.randint(0, 10) for _ in range(5)]
+    vecTest2 = [1, 1, 1, 1, 1]
+
+    print("Vecteur :", vecTest1, "\tEntropie :", entropie(vecTest1))
+    print("Vecteur :", vecTest2, "\tEntropie :", entropie(vecTest2))

73b7925d33f2da27c77cdcafa4017bd7b800ba06
unknown <almehdi.krisni@gmail.com>
2022-03-22 02:20:10 +0100

    Decision Tree pdf creation - TME1

--- TME1/methodesTME.py
+++ TME1/methodesTME.py
@@ -2,10 +2,12 @@
 # Imports
 #############################################################################################
 
+from ast import If
 import numpy as np
 import pickle
 from collections import Counter
 import math
+import matplotlib.pyplot as plt
 import time
 import random
 
@@ -49,18 +51,43 @@ datay = np.array([1 if x [33] > 6.5 else -1 for x in data ])
 from sklearn.tree import export_graphviz
 from sklearn.tree import DecisionTreeClassifier as DTree
 import pydotplus
+# L'affichage des pdf grâce à cette source et la librairie plot_tree
+from sklearn.tree import plot_tree
 
 id2genre = [x[1] for x in sorted(fields.items())[: -2]]
 dt = DTree()
-dt.max_depth = 5 # on fixe la taille max de l ’ arbre a 5
-dt.min_samples_split = 2 # nombre minimum d ’ exemples pour spliter un noeud
+dt.max_depth = 5 # on fixe la taille max de l’arbre a 5
+dt.min_samples_split = 2 # nombre minimum d’exemples pour spliter un noeud
 dt.fit(datax, datay)
 dt.predict(datax[:5,:])
 # print(dt.score(datax, datay))
 # utiliser http://www.webgraphviz.com/ par exemple ou https://dreampuf.github.io/Graphvizexport_graphviz ( dt , out_file ="/tmp / tree . dot ", feature_names = id2genre )
 # ou avec pydotplus
-tdot = export_graphviz(dt, feature_names = id2genre )
-pydotplus.graph_from_dot_data(tdot).write_pdf("tree.pdf")
+# tdot = export_graphviz(dt, feature_names = id2genre)
+# pydotplus.graph_from_dot_data(tdot).write_pdf("tree.pdf")
+
+# Méthode permettant la création d'un arbre de décision de profondeur précise
+# avec un apprentissage sur les données passées en paramètres
+def treeMaker(maxDepth, X, Y, doSave=True, doPlot=False) :
+    # On crée l'arbre avec une profondeur précise
+    dt = DTree(max_depth = maxDepth, min_samples_split = 2)
+
+    # On entraine l'arbre et on le retourne
+    dt.fit(X, Y)
+
+    # Affichage de la précision de l'arbre
+    print("Précision de l'arbre de décision avec une profondeur maximale de", maxDepth, ":", dt.score(X, Y))
+
+    # Sauvegarde de l'arbre en fichier pdf
+    tdot = export_graphviz(dt, feature_names = id2genre)
+    fileN = "DTreeImages/tree_max_depth_" + str(maxDepth) + ".pdf"
+    pydotplus.graph_from_dot_data(tdot).write_pdf(fileN)
+
+    # Affichage de l'arbre
+    if (doPlot) :
+        plt.figure(figsize=(maxDepth * 5, maxDepth * 5))
+        plot_tree(dt, feature_names = list(fields.values())[:32], filled=True)
+        plt.show()
 
 #############################################################################################
 # Partie EXEC - Tests

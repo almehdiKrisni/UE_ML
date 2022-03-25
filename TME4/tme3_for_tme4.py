f717cf36db734a0bd054ac8495e0bfc0f5990ea6
unknown <almehdi.krisni@gmail.com>
2022-03-25 02:09:25 +0100

    Methode fit mise à jour - TME4

---
+++ TME4/tme3_for_tme4.py
@@ -0,0 +1,215 @@
+#################################################################################################
+# Imports
+#################################################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mltools import plot_data, plot_frontiere, make_grid, gen_arti
+
+#################################################################################################
+# MEAN SQUARE ERROR
+#################################################################################################
+
+def mse(w,x,y):
+    # On fait attention à ce que les données soit dans la bonne dimension
+    y = y.reshape(-1, 1)
+    w = w.reshape(-1, 1)
+    # Autant d'exemples que de labels, autant d'attributs que de poids dans w
+    x = x.reshape(y.shape[0], w.shape[0]) 
+
+    # On retourne le coût aux moindres carrés
+    return ((np.dot(x, w) - y) ** 2)
+
+def mse_grad(w,x,y):
+    # On fait attention à ce que les données soit dans la bonne dimension
+    y = y.reshape(-1, 1)
+    w = w.reshape(-1, 1)
+    # Autant d'exemples que de labels, autant d'attributs que de poids dans w
+    x = x.reshape(y.shape[0], w.shape[0]) 
+    
+    # On retourne le gradient
+    return (-2) * x * (y - np.dot(x,w))
+
+#################################################################################################
+# LOGISTIC REGRESSION
+#################################################################################################
+
+def reglog(w,x,y):
+    # On fait attention à ce que les données soit dans la bonne dimension
+    y = y.reshape(-1, 1)
+    w = w.reshape(-1, 1)
+    # Autant d'exemples que de labels, autant d'attributs que de poids dans w
+    x = x.reshape(y.shape[0], w.shape[0]) 
+    
+    # On retourne le coût de la reg. logistique
+    return np.log(1 + np.exp((-y) * np.dot(x,w)))
+
+def reglog_grad(w,x,y):
+    # On fait attention à ce que les données soit dans la bonne dimension
+    y = y.reshape(-1, 1)
+    w = w.reshape(-1, 1)
+    # Autant d'exemples que de labels, autant d'attributs que de poids dans w
+    x = x.reshape(y.shape[0], w.shape[0]) 
+    
+    # On retourne le gradient
+    return ((- y) * x) / (1 + np.exp(y * np.dot(x,w)))
+
+#################################################################################################
+# Méthode supplémentaire de vérification
+#################################################################################################
+
+def check_fonctions():
+    ## On fixe la seed de l'aléatoire pour vérifier les fonctions
+    np.random.seed(0)
+    datax, datay = gen_arti(epsilon=0.1)
+    wrandom = np.random.randn(datax.shape[1],1)
+    assert(np.isclose(mse(wrandom,datax,datay).mean(),0.54731,rtol=1e-4))
+    assert(np.isclose(reglog(wrandom,datax,datay).mean(), 0.57053,rtol=1e-4))
+    assert(np.isclose(mse_grad(wrandom,datax,datay).mean(),-1.43120,rtol=1e-4))
+    assert(np.isclose(reglog_grad(wrandom,datax,datay).mean(),-0.42714,rtol=1e-4))
+    np.random.seed()
+
+# On pourrait se servir de la méthode de génération fournie dans mltools mais on préfère générer des
+# ensembles totalement aléatoires
+# Méthode de test d'exactitude
+def grad_check(f, f_grad, N=100, eps=1E-2) :
+    # Tirage aléatoire des exemples et de w
+    x = np.array([np.random.randint(0,100) for i in range(N)]).reshape(-1,1)
+    w = np.array([np.random.randint(1,10)])
+    y = np.array(np.random.choice([-1,1]))
+
+    # Variable conservant la différence moyenne des gradients
+    diffGrad = 0.
+
+    # On sauvegarde même les valeurs calculées
+    logGrad = []
+
+    # On parcourt les exemples
+    for i in range(N) :
+        # On calcule les gradients
+        grad = f_grad(w + eps, x[i], y)
+        gradT = ((f(w + eps, x[i], y)) - (f(w,x[i],y))) / eps
+
+        # On calcule la différence et on l'ajoute à diffGrad
+        diffGrad += np.abs(grad - gradT)
+
+        # On sauvegarde les valeurs
+        logGrad.append((grad[0][0], gradT[0][0]))
+        
+
+    # On retourne la différence et le log
+    return (diffGrad / N), logGrad
+
+#################################################################################################
+# Descente de gradient
+#################################################################################################
+
+# Descente de gradient batch
+def descente_gradient_batch(datax, datay, f_loss, f_grad, eps, maxIter=100, plot=False) :
+    # On crée le w initial et les listes de sauvegardes des w et de la fonction de coût
+    w = np.zeros((1, len(datax[0])))
+    logW = [w[0].tolist()]
+    logF = [np.mean(f_loss(w, datax, datay))]
+
+    # On itère sur le nombre maximal d'itérations
+    for iter in range(maxIter) :
+        d = np.mean(f_grad(w, datax, datay), axis=0)
+        w -= (eps * d)
+
+        # On sauvegarde les valeurs
+        logW.append(w[0].tolist())
+        logF.append(np.mean(f_loss(w, datax, datay)))
+
+    # Affichage si demandé
+    if (plot) :
+        # Affichage de l'évolution de la perte
+        plt.figure()
+        plt.title("Evolution de la loss en fonction du nombre d'itérations en mode batch\n(eps = " + str(eps) + " , maxIter = " + str(maxIter) + ")")
+        plt.ylabel("Loss")
+        plt.xlabel("Itération")
+        plt.plot([(i + 1) for i in range(maxIter + 1)], logF,'r')
+        plt.show()
+
+    # On retourne w et les listes
+    return w, logW, logF
+
+# Descente de gradient stochastique
+def descente_gradient_stoch(datax, datay, f_loss, f_grad, eps, maxIter=100, plot=False) :
+    # On crée le w initial et les listes de sauvegardes des w et de la fonction de coût
+    w = np.zeros((1, len(datax[0])))
+    logW = [w[0].tolist()]
+    logF = [np.mean(f_loss(w, datax, datay))]
+
+    # On itère sur le nombre maximal d'itérations
+    for iter in range(maxIter) :
+        index = np.random.randint(0, len(datax) - 1)
+        w -= (eps * f_grad(w, datax[index], datay[index]))
+
+        # On sauvegarde les valeurs
+        logW.append(w[0].tolist())
+        logF.append(np.mean(f_loss(w, datax, datay)))
+
+    # Affichage si demandé
+    if (plot) :
+        # Affichage de l'évolution de la perte
+        plt.figure()
+        plt.title("Evolution de la loss en fonction du nombre d'itérations en mode stochastique\n(eps = " + str(eps) + " , maxIter = " + str(maxIter) + ")")
+        plt.ylabel("Loss")
+        plt.xlabel("Itération")
+        plt.plot([(i + 1) for i in range(maxIter + 1)], logF,'r')
+        plt.show()
+
+    # On retourne w et les listes
+    return w, logW, logF
+
+# Descente de gradient mini-batch
+def descente_gradient_mini(datax, datay, f_loss, f_grad, eps, part,  maxIter=100, plot=False) :
+    # On crée le w initial et les listes de sauvegardes des w et de la fonction de coût
+    w = np.zeros((1, len(datax[0])))
+    logW = [w[0].tolist()]
+    logF = [np.mean(f_loss(w, datax, datay))]
+
+    # On itère sur le nombre maximal d'itérations
+    for iter in range(maxIter) :
+        ind = [np.random.randint(0, len(datax) - 1) for i in range(part)] # Indices des exemples à utiliser
+        d = np.mean([f_grad(w, datax[i], datay[i]) for i in ind])
+        w -= (eps * d)
+
+        # On sauvegarde les valeurs
+        logW.append(w[0].tolist())
+        logF.append(np.mean(f_loss(w, datax, datay)))
+
+    # Affichage si demandé
+    if (plot) :
+        # Affichage de l'évolution de la perte
+        plt.figure()
+        plt.title("Evolution de la loss en fonction du nombre d'itérations en mode mini-batch\n(eps = " + str(eps) + " , maxIter = " + str(maxIter) + ", part = " + str(part) + ")")
+        plt.ylabel("Loss")
+        plt.xlabel("Itération")
+        plt.plot([(i + 1) for i in range(maxIter + 1)], logF,'r')
+        plt.show()
+
+    # On retourne w et les listes
+    return w, logW, logF
+
+#################################################################################################
+# Main
+#################################################################################################
+
+if __name__=="__main__":
+    ## Tirage d'un jeu de données aléatoire avec un bruit de 0.1
+    datax, datay = gen_arti(epsilon=0.1)
+    ## Fabrication d'une grille de discrétisation pour la visualisation de la fonction de coût
+    grid, x_grid, y_grid = make_grid(xmin=-2, xmax=2, ymin=-2, ymax=2, step=100)
+    
+    plt.figure()
+    ## Visualisation des données et de la frontière de décision pour un vecteur de poids w
+    w  = np.random.randn(datax.shape[1],1)
+    plot_frontiere(datax,lambda x : np.sign(x.dot(w)),step=100)
+    plot_data(datax,datay)
+
+    ## Visualisation de la fonction de coût en 2D
+    plt.figure()
+    plt.contourf(x_grid,y_grid,np.array([mse(w,datax,datay).mean() for w in grid]).reshape(x_grid.shape),levels=20)
+    
--- TME4/tme4.py
+++ TME4/tme4.py
@@ -1,9 +1,47 @@
+#################################################################################################
+# Imports
+#################################################################################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mltools import plot_data, plot_frontiere, make_grid, gen_arti
+from tme3_for_tme4 import *
 
-    
+#################################################################################################
+# Perceptron
+#################################################################################################
+
+def perceptron_loss(w,x,y) :
+    # Comme dans le TME3
+    # On fait attention à ce que les données soit dans la bonne dimension
+    y = y.reshape(-1, 1)
+    w = w.reshape(-1, 1)
+    # Autant d'exemples que de labels, autant d'attributs que de poids dans w
+    x = x.reshape(y.shape[0], w.shape[0])
+
+    # On retourne la loss du perceptron
+    return np.maximum(0, - (y * np.dot(x,w)))
+
+def perceptron_grad(w,x,y) :
+    # On fait attention à ce que les données soit dans la bonne dimension
+    y = y.reshape(-1, 1)
+    w = w.reshape(-1, 1)
+    # Autant d'exemples que de labels, autant d'attributs que de poids dans w
+    x = x.reshape(y.shape[0], w.shape[0])
+
+    # Le perceptron ne réalise une correction, donc on cherche les points mal classés
+    classif = (y * np.dot(x,w)).flatten()
+    indexBadClassif = np.where(classif < 0)
+
+    # On crée le gradient à retourner
+    grad = np.zeros(len(x))
+    grad[indexBadClassif] = ((-y) * x)[indexBadClassif]
+    return grad
+
+#################################################################################################
+# IClasse Linéaire
+#################################################################################################
 
 class Lineaire(object):
     def __init__(self,loss=perceptron_loss,loss_g=perceptron_grad,max_iter=100,eps=0.01):
@@ -11,14 +49,45 @@ class Lineaire(object):
         self.w = None
         self.loss,self.loss_g = loss,loss_g
         
-    def fit(self,datax,datay):
-        pass
+    def fit(self,datax,datay,mode="batch",part=10):
+        # On sauvegarder les ensembles d'apprentissage
+        self.trainx = datax
+        self.trainy = datay
+
+        # On retourne la descente de gradient
+        if (mode == "batch") :
+            self.w, self.wLog, self.fLog = descente_gradient_batch(self.trainx, self.trainy, self.loss, self.loss_g, eps=self.eps, maxIter=self.max_iter)
+        
+        elif (mode == "stoch") :
+            self.w, self.wLog, self.fLog = descente_gradient_stoch(self.trainx, self.trainy, self.loss, self.loss_g, eps=self.eps, maxIter=self.max_iter)
+
+        elif (mode == "mini") :
+            self.w, self.wLog, self.fLog = descente_gradient_mini(self.trainx, self.trainy, self.loss, self.loss_g, eps=self.eps, part=part, maxIter=self.max_iter)
+
+        # Aucun mode ne correspond
+        else :
+            print("Le paramètre 'mode' n'a pas été reconnu. Veuillez le modifier.")
 
     def predict(self,datax):
-        pass
+        # On sauvegarder l'ensemble de test
+        self.testx = datax
+
+        # On retourne la prédiction avec le signe du produit scalaire
+        return np.sign(np.dot(self.testx, self.w.reshape(-1,1)))
 
     def score(self,datax,datay):
-        pass
+        # On reourne le score de classification sur les données passées en paramètres
+        # On sauvegarde les ensembles
+        self.testx = datax
+        self.testy = datay
+
+        # On prédit les labels de datax et on retourne la précision
+        pred = self.predict(self.testx)
+        return np.mean([1 if (self.testy[i] == pred[i]) else 0 for i in range(len(self.testy))])
+
+#################################################################################################
+# Gestion données USPS
+#################################################################################################
 
 def load_usps(fn):
     with open(fn,"r") as f:
@@ -39,7 +108,9 @@ def get_usps(l,datax,datay):
 def show_usps(data):
     plt.imshow(data.reshape((16,16)),interpolation="nearest",cmap="gray")
 
-
+#################################################################################################
+# Main
+#################################################################################################
 
 if __name__ =="__main__":
     uspsdatatrain = "../data/USPS_train.txt"
